34d0b99928d1fa9e16f04674560e80ee36b3e9ce
OB5-DEV <obadaot50@gmail.com>
2023-09-30 22:56:12 +0200

    chore: change site name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ahmads Blog';
+export const SITE_TITLE = 'Eng. Ahmad Haileh';
 export const SITE_DESCRIPTION = 'Welcome to my protolio/blog!';
