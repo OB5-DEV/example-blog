77378c6dd66849a348cccf072b136bc29afd51e6
OB5-DEV <obadaot50@gmail.com>
2023-09-30 22:38:48 +0200

    chore: add projects and research

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,4 +12,30 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+const research = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+export const collections = { blog, project, research };
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function GET(context) {
+export async function GETblog(context) {
 	const posts = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
@@ -11,6 +11,32 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
+		})),
+	});
+}
+
+export async function GETproject(context) {
+	const posts = await getCollection('project');
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/project/${post.slug}/`,
+		})),
+	});
+}
+
+export async function GETresearch(context) {
+	const posts = await getCollection('research');
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/research/${post.slug}/`,
 		})),
 	});
 }
